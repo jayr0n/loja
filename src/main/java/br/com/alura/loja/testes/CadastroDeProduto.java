36e27d93c66e86a09a75441467bfd74cff0bddb6
jayron88 <jayron@hotmail.com.br>
2023-09-02 12:27:11 -0300

    projeto finalizado

---
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,55 @@
+package br.com.alura.loja.testes;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+public class CadastroDeProduto {
+	
+	public static void main(String[] args) {
+        cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        
+        Produto p = produtoDao.buscarPorId(1l);
+        
+        System.out.println(p.getNome() + p.getDescricao() + p.getPreco() + p.getId());
+        
+        List<Produto> todos= produtoDao.buscarTodos();
+        todos.forEach(p2 -> System.out.println(p.getDescricao()));
+        
+        List<Produto> todosNome= produtoDao.buscarPorNome("Xiaomi Redmi");
+        todosNome.forEach(p2 -> System.out.println(p.getNome()));
+        
+        List<Produto> nomeCategoria= produtoDao.buscarPorNomeDaCategoria("CELULARES");
+        nomeCategoria.forEach(p2 -> System.out.println(p.getNome()));
+        
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+        System.out.println("Preço do produto: " + precoDoProduto);
+    }
+
+	private static void cadastrarProduto() {
+		Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+
+        em.getTransaction().begin();
+
+        categoriaDao.cadastrar(celulares);
+        produtoDao.cadastrar(celular);
+
+        em.getTransaction().commit();
+        em.close();
+	}
+}
+
